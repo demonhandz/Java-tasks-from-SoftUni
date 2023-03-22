987b9878c2d360124b2ca6e9055b12294d6416e7
demonhandz <demonhandz@gmail.com>
2023-03-22 23:00:28 +0200

    task completed

--- src/WhileLoop/Graduation_08.java
+++ src/WhileLoop/Graduation_08.java
@@ -10,21 +10,30 @@ public class Graduation_08 {
         int goodGradesCount = 0;
         int poorGradesCount = 0;
         double allGradesSum = 0;
+        int gradeCount = 1;
+        boolean isFailed = false;
 
         while (goodGradesCount < 12) {
-            double grade = Double.parseDouble(scanner.nextLine());
 
             if (poorGradesCount == 2) {
-
+                isFailed = true;
                 break;
             }
+            double grade = Double.parseDouble(scanner.nextLine());
 
             if (grade < 4) {
                 poorGradesCount++;
-                grade = Double.parseDouble(scanner.nextLine());
                 continue;
             }
+            gradeCount++;
+            goodGradesCount++;
             allGradesSum += grade;
+        }
+        double avgGrade = allGradesSum / 12;
+        if (isFailed) {
+            System.out.printf("%s has been excluded at %d grade", student, gradeCount);
+        } else {
+            System.out.printf("%s graduated. Average grade: %.2f", student, avgGrade);
         }
     }
 }
